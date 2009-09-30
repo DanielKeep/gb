df689e1c8a0a0f85bf5329f8d3f88ef207c68a70
Daniel Keep <daniel.keep@gmail.com>
2009-09-30 19:40:07 +1000

    Added overloads for util.Tuple.Sequence.
    Also added docs for it.

--- src/gb/util/Tuple.d
+++ src/gb/util/Tuple.d
@@ -8,6 +8,17 @@ module gb.util.Tuple;
 
 import tango.core.Tuple;
 
+/**
+ * Sequence can be used to generate a tuple of integer values.  It can be
+ * used in one of three forms:
+ *
+ * -----
+ *  Sequence!(6)     // ==> (0, 1, 2, 3, 4, 5)
+ *  Sequence!(1,6)   // ==> (1, 2, 3, 4, 5)
+ *  Sequence!(1,6,2) // ==> (1, 3, 5)
+ * -----
+ */
+
 template Sequence(int max)
 {
     static if( max <= 0 )
@@ -16,10 +27,38 @@ template Sequence(int max)
         alias Tuple!(Sequence!(max-1), max-1) Sequence;
 }
 
+/// ditto
+
+template Sequence(int min, int max)
+{
+    static if( max <= min )
+        alias Tuple!() Sequence;
+    else
+        alias Tuple!(Sequence!(min, max-1), max-1) Sequence;
+}
+
+/// ditto
+
+template Sequence(int min, int max, int stride)
+{
+    static if( min >= max )
+        alias Tuple!() Sequence;
+    else
+        alias Tuple!(min, Sequence!(min+stride, max, stride)) Sequence;
+}
+
 version( Unittest )
 {
     static assert( Sequence!(3)[0] == 0 );
     static assert( Sequence!(3)[1] == 1 );
     static assert( Sequence!(3)[2] == 2 );
     static assert( Sequence!(3).length == 3 );
+
+    static assert( Sequence!(1, 3)[0] == 1 );
+    static assert( Sequence!(1, 3)[1] == 2 );
+    static assert( Sequence!(1, 3).length == 2 );
+
+    static assert( Sequence!(1, 5, 2)[0] == 1 );
+    static assert( Sequence!(1, 5, 2)[1] == 3 );
+    static assert( Sequence!(1, 5, 2).length == 2 );
 }
