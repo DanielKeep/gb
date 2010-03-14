32d545a9789cf6806c3e502cd1ad348c1ad98dc9
Daniel Keep <daniel.keep@gmail.com>
2010-03-14 16:46:05 +1100

    Added util.Endianess.

---
+++ src/gb/util/Endianness.d
@@ -0,0 +1,18 @@
+/**
+ * Contains stuff related to Endianness.
+ *
+ * Authors: Daniel Keep <daniel.keep@gmail.com>
+ * Copyright: See LICENSE.
+ */
+module gb.util.Endianness;
+
+/**
+ * Used to encode endianness.  Defaults to an ``Auto`` value.
+ */
+enum Endianness
+{
+    Auto,
+    Big,
+    Little,
+}
+
