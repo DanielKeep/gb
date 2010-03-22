82cdbe11f082379623fb01400e2460043d7d98df
Daniel Keep <daniel.keep@gmail.com>
2010-03-23 10:29:35 +1100

    Changed run-all.cmd to be a easier to read.

--- src/gb/tools/HashTest/run-all.cmd
+++ src/gb/tools/HashTest/run-all.cmd
@@ -1,7 +1,10 @@
 @ECHO OFF
 
-set FLAGS=--cpu-info --timed 60
-set TESTS=+i-i-r 100 +i-i-r 100000 +i-i-seq 100 +i-i-seq 100000 +i-i-seq 200000 2 +i-s-seq 100 +i-s-seq 100000 +i-s-seq 200000 2
+set FLAGS=--cpu-info
+set TESTS=
+set TESTS=%TESTS% +i-i-seq 100 +i-i-seq 100000 +i-i-seq 200000 2
+set TESTS=%TESTS% +i-s-seq 100 +i-s-seq 100000 +i-s-seq 200000 2
+set TESTS=%TESTS% +i-i-r 100 +i-i-r 1000001
 
 del *.log
 
